4357827f685b6f1d780bcc3f8c2b6eca9075e056
cafreeman <cfreeman@alteryx.com>
2015-12-17 09:14:58 -0600

    Scala - Word Count

--- scala/word-count/src/main/scala/wordCount.scala
+++ scala/word-count/src/main/scala/wordCount.scala
@@ -1,7 +1,13 @@
 class Phrase(val p: String) {
   def wordCount: Map[String, Int] = {
-    return p.split(' ')
-      .groupBy(_.distinct)
+    return p.split(Array(' ', ','))
+      .map(
+        _.filter(x => {
+          x.isLetterOrDigit || x == '\''
+        }).toLowerCase
+      )
+      .groupBy(x => x)
+      .filter(x => x._1.nonEmpty)
       .map(x => x._1 -> x._2.length)
   }
 }
